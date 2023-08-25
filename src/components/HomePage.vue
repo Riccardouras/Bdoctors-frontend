dca8921a846d3074a8704e5d3aa0e8665bf533f6
luca betti sorbelli <luca betti sorbelli>
2023-08-25 15:39:52 +0200

    add card dottori in evidenza

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,7 +8,17 @@ export default {
       images: [
         'src/img/logo.png',
         'src/img/programmazione.webp'
-      ]
+      ],
+      doctors: [
+        { id: 1, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/logo.png' },
+        { id: 2, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/programmazione.webp' },
+        { id: 3, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/programmazione.webp' },
+        { id: 4, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/programmazione.webp' },
+        { id: 5, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/programmazione.webp' },
+        { id: 6, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/programmazione.webp' },
+        { id: 7, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/logo.png' },
+        { id: 8, name: 'Nome', surname: 'Cognome', specialization: 'Specializzazione', image: 'src/img/programmazione.webp' },
+      ],
     }
   },
   methods: {
@@ -25,14 +35,18 @@ export default {
   <!-- NAVBAR -->
   <header>
     <div class="container-1">
-      <div class="nav-left">
-        <img id="logo" src="../img/LogoPiccolo.png" alt="">
-        <h1>B-Doctors</h1>
-      </div>
-      <div class="nav-right">
-        <a href="">Registrati</a>
-        <a href="">Accedi</a>
-      </div>
+      <Motion :initial="{ x: -1000 }" :animate="{ x: 0 }" :transition="{ type: 'spring', stiffness: 100 }">
+        <div class="nav-left">
+          <img id="logo" src="../img/LogoPiccolo.png" alt="">
+          <h1>B-Doctors</h1>
+        </div>
+      </Motion>
+      <Motion :initial="{ x: 1000 }" :animate="{ x: 0 }" :transition="{ type: 'spring', stiffness: 100 }">
+        <div class="nav-right">
+          <a href="">Registrati</a>
+          <a href="">Accedi</a>
+        </div>
+      </Motion>
     </div>
   </header>
   <main>
@@ -55,35 +69,20 @@ export default {
       </div>
     </form>
     <!-- MEDICI IN EVIDENZA -->
-    <div class="container-fluid">
+    <div class="container">
       <div class="row">
-        <div class="sponsorDoctors">
-          <h2>Dottori in evidenza</h2>
-        </div>
-        <div class="col-7 bg-light pt-4 d-flex flex-column align-items-center border-end">
-          <Motion :initial="{ x: -1000 }" :animate="{ x: 0 }" :transition="{ type: 'spring', stiffness: 100 }">
-            <div class="cardImages" style="height: 700px">
-              <img class="img-fluid h-100" :src="images[currentImage]" alt="Immagine del carosello" />
+        <h2 class="text-center">Dottori in evidenza</h2>
+        <div class="col-sm-3 mt-2" v-for="doctor in doctors" :key="doctor.id">
+          <div class="card">
+            <img class="card-img-top" :src="doctor.image" :alt="doctor.name">
+            <div class="card-body">
+              <h5 class="card-title">{{ doctor.name }} {{ doctor.surname }}</h5>
+              <p class="card-text">{{ doctor.specialization }}</p>
             </div>
-            <div class="buttons text-primary p-4 w-48 d-flex justify-content-evenly">
-              <button class="btn btn-primary" @click="prevImage">Precedente</button>
-              <button class="btn btn-primary" @click="nextImage">Successivo</button>
-            </div>
-          </Motion>
-        </div>
-        <div class="col-5 bg-light p-4 text-center d-flex justify-content-around flex-column">
-          <Motion :initial="{ x: 1000 }" :animate="{ x: 0 }" :transition="{ type: 'spring', stiffness: 100 }">
-            <h2 class="text-primary">Descrizione Dottore</h2>
-            <div class="text-primary description d-flex justify-content-around flex-column">
-              <span>Nome:</span>
-              <span>Cognome:</span>
-              <span>Specializzazione:</span>
-            </div>
-          </Motion>
+          </div>
         </div>
       </div>
     </div>
-    <!-- MEDICI IN EVIDENZA -->
   </main>
 </template>
 
