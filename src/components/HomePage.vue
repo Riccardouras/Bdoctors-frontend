3a382fbcca13865b98f8478b2cdb222659269f6f
luca betti sorbelli <luca betti sorbelli>
2023-08-28 09:51:23 +0200

    add FAQ accordion

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -48,10 +48,10 @@ export default {
             <a href="">Accedi</a>
           </div>
         </Motion>
-
       </div>
-      <div class="container">
 
+      <!-- FORM RICERCA -->
+      <div class="container">
         <div class="titleDoctor d-flex flex-column justify-content-around w-75 m-auto align-items-start pt-5 ">
           <h2>Cerca il tuo dottore!</h2>
           <h4>Cerca tra 100 000 Specialisti e Medici di Medicina Generale.</h4>
@@ -74,12 +74,8 @@ export default {
 
     </div>
   </header>
+
   <main>
-    <!-- FORM SPECIALIZZAZIONE -->
-
-
-
-
     <!-- MEDICI IN EVIDENZA -->
     <div class="container">
       <div class="row">
@@ -96,6 +92,78 @@ export default {
       </div>
     </div>
   </main>
+
+  <footer>
+    <!-- FAQ -->
+    <div class="container">
+      <div class="titleFaq text-center mt-4 mb-4">
+        <h2>F.A.Q.</h2>
+      </div>
+      <div class="accordion mt-4 mb-4" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+              aria-expanded="true" aria-controls="collapseOne">
+              Come posso prenotare una visita con un dottore?
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              Puoi prenotare una visita con un dottore utilizzando il nostro sistema di prenotazione online o chiamando
+              il nostro numero di telefono. Basta selezionare il dottore che desideri visitare e scegliere una data e un
+              orario disponibili.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Quali sono i metodi di pagamento accettati?
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              Accettiamo pagamenti tramite carta di credito, bonifico bancario e contanti. Si prega di notare che alcuni
+              dottori potrebbero avere metodi di pagamento preferiti, quindi si consiglia di verificare con loro prima
+              della visita.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Cosa devo portare con me alla visita?
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              Si consiglia di portare con sé la tessera sanitaria, eventuali documenti medici rilevanti e una lista dei
+              farmaci che si stanno assumendo. Inoltre, potrebbe essere utile portare una lista di domande o
+              preoccupazioni da discutere con il dottore durante la visita.
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Come posso annullare o modificare un appuntamento?
+              </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                Puoi annullare o modificare un appuntamento utilizzando il nostro sistema di prenotazione online o
+                chiamando il nostro numero di telefono. Si prega di notare che potrebbero essere applicate delle penali
+                per le cancellazioni effettuate con meno di 24 ore di preavviso.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </footer>
 </template>
 
 <style lang="scss">
