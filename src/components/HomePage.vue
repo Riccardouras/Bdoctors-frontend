8834684f87e3d3e9dec71d330e179f585afd0c56
Francesco Meloni <francesco.meloni03@icloud.com>
2023-08-29 11:44:37 +0200

    fix

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -158,34 +158,27 @@ export default {
         </div>
       </div>
     </div>
-<<<<<<< HEAD
-
-
-
-
-
     <div class=" mt-5">
-
-=======
-    <!-- LINK SOCIAL -->
-    <div class=" mt-5">
->>>>>>> Advanced-Page
-      <div class="bg-dark text-center text-white">
-        <div class="container p-4 pb-0">
-          <section class="mb-4">
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                class="fab fa-facebook-f"></i></a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                class="fab fa-linkedin-in"></i></a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
-          </section>
-        </div>
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-          © 2023 Copyright:
-          <a class="text-white" href="https://mdbootstrap.com/">BooleanProject</a>
+      <!-- LINK SOCIAL -->
+      <div class=" mt-5">
+        <div class="bg-dark text-center text-white">
+          <div class="container p-4 pb-0">
+            <section class="mb-4">
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                  class="fab fa-facebook-f"></i></a>
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                  class="fab fa-instagram"></i></a>
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                  class="fab fa-linkedin-in"></i></a>
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+            </section>
+          </div>
+          <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2023 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">BooleanProject</a>
+          </div>
         </div>
       </div>
     </div>
@@ -213,15 +206,8 @@ header {
   align-items: center;
   width: 330px;
   justify-content: space-between;
-<<<<<<< HEAD
-
-
 }
 
-
-=======
-}
->>>>>>> Advanced-Page
 
 #logo {
   width: 110px;
